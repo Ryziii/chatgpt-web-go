00c19041a98ba1c4526b104650afe1c9e2ee4621
rysiw <az.convict@hotmail.com>
2023-04-30 14:47:28 +0800

    update: 深拷贝

--- utils/copy.go
+++ utils/copy.go
@@ -1,6 +1,9 @@
 package utils
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"github.com/jinzhu/copier"
+)
 
 func DeepCopyByJson(src interface{}, dst interface{}) error {
 	if tmp, err := json.Marshal(src); err != nil {
@@ -9,4 +12,10 @@ func DeepCopyByJson(src interface{}, dst interface{}) error {
 		err := json.Unmarshal(tmp, dst)
 		return err
 	}
+}
+func DeepCopy(from interface{}, to interface{}) error {
+	if err := copier.CopyWithOption(to, from, copier.Option{IgnoreEmpty: true, DeepCopy: true}); err != nil {
+		return err
+	}
+	return nil
 }
